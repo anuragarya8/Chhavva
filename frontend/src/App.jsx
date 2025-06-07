337daf0f0dc4953e82982060ff9aca93cfc0988e
Anurag Arya <kumaranurag296@gmail.com>
2025-06-07 18:54:21 +0530

    Add initial frontend structure with assets, components, and pages
    - Added various image assets including logos and icons.
    - Implemented Navbar component with responsive design and dropdown menu.
    - Created main application structure with routing using React Router.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './index.css'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Product from './pages/Product'
+import Collection from './pages/Collection'
+import Cart from './pages/Cart'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+
+      <Navbar/>
+
+      
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/product/:productId' element={<Product/>} />
+        <Route path='/collection' element={<Collection/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/placeorder' element={<PlaceOrder/>} />
+        <Route path='/orders' element={<Orders/>} />
+      </Routes>
+    
+    </div>
+  )
+}
+
+export default App
